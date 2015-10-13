59a192a4b937acbe8347ca00d96c0c4e0176f94e
Davide Gessa <gessadavide@gmail.com>
2015-10-13 14:39:24 +0200

    dapp code

--- dapp/proto.py
+++ dapp/proto.py
@@ -3,6 +3,6 @@
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 class BlockStoreProto:
-	DAPP_CODE = 0x08
+	DAPP_CODE = [ 0x01, 0x02 ]
 	METHOD_SET = 0x01
 	METHOD_LIST = [METHOD_SET]
