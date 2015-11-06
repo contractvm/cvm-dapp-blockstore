f6766a458fd505ea74ff02a9d8e7ddddfb5dfc8a
Davide Gessa <gessadavide@gmail.com>
2015-11-06 11:53:41 +0100

    helloworld

--- samples/helloworld.py
+++ samples/helloworld.py
@@ -3,7 +3,7 @@
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-from libcontractvm import Wallet, WalletChainSo, ConsensusManager
+from libcontractvm import Wallet, WalletNode, ConsensusManager
 from blockstore import BlockstoreManager
 import sys
 import config
